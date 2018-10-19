1e43864ed5396b98aa08d44cdec8cd1b10df5ce6
pyrotechRio <riowinslow@gmail.com>
2018-10-18 21:15:12 -0400

    just to prove 2

--- Robot1.ino
+++ Robot1.ino
@@ -8,7 +8,7 @@
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-
+ 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 Servo leftW;
 Servo rightW;
@@ -53,4 +53,3 @@ void loop() {
   }
 
 }
-
